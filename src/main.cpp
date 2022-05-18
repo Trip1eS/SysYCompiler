01f962209e60d90164067b02bfe4a3cdbc7d1948
Shushun Shi <shiss2001@outlook.com>
2022-05-18 18:59:30 +0800

    feat: added support for command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "Lexer.hpp"
 #include "Token.hpp"
 #include "Parser.hpp"
+#include "IrGenerator.hpp"
 
-int main(int, char **) {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "error: no input file\n";
+        return 1;
+    }
+    std::string inFilePath(argv[1]);
+    std::string outFilePath;
+    if (argc >= 3 && strcmp(argv[2], "-o") == 0) {
+        if (argc == 4) {
+            outFilePath = argv[3];
+        } else {
+            std::cerr << "error: no output file\n";
+            return 1;
+        }
+    } else {
+        outFilePath = "out.s";
+    }
+
     Lexer lexer;
-    auto [tokens, errors] = lexer.lex("../../samples/sysy.sy");
+    auto [tokens, errors] = lexer.lex(inFilePath);
     Parser parser(std::move(tokens));
     parser.parse();
+    IrGenerator irGen(std::move(parser.getCompUnits()));
+    irGen.codegen();
+    irGen.output(outFilePath, llvm::CGFT_AssemblyFile);
 
     return 0;
 }
